edb03769fb25a8cfa4d0c9d57eff01c8abd7e033
The Great Macraft <156778003+TheGreatMacraft@users.noreply.github.com>
2024-01-21 19:32:27 +0100

    Add files via upload

--- SDL/src/main.cpp
+++ SDL/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 class Player{
     public:
@@ -24,8 +25,8 @@ void Player::move(int x, int y)
 
 void Player::setPosition(int x, int y)
 {
-    object.x = x;
-    object.y = y;
+    object.x = x - object.w/2;
+    object.y = y - object.h/2;
 }
 
 void Player::draw(SDL_Renderer *renderer)
@@ -67,10 +68,22 @@ int main( int argc, char* argv[] )
     SDL_Event event;
 
     Player player;
-    player.size(20,20);
-    player.setPosition(WIDTH/2 - player.object.w/2, HEIGHT/2 - player.object.h/2);
+    player.size(10,10);
+    player.setPosition(WIDTH/2, HEIGHT/2);
+
+    Player enemy;
+    enemy.size(10,10);
+    enemy.setPosition(WIDTH/2, HEIGHT/3);
+
+    SDL_Texture *playerImage = IMG_LoadTexture(renderer, "Player.png");
+    SDL_QueryTexture(playerImage, NULL, NULL, &player.object.w, &player.object.h);
+
+    SDL_Texture *enemyImage = IMG_LoadTexture(renderer,"enemy.png");
+    SDL_QueryTexture(enemyImage, NULL, NULL, &enemy.object.w, &enemy.object.h);
 
     int moveX, moveY, moveAmount = 10;
+    int enemyMoveAmount = 5;
+    int enemyFollowX = enemyMoveAmount, enemyFollowY = enemyMoveAmount;
 
     bool running = true;
     while (running)
@@ -102,14 +115,39 @@ int main( int argc, char* argv[] )
             }
 
         }
-
         SDL_RenderClear(renderer);
-        player.draw(renderer);
+        SDL_RenderCopy(renderer, enemyImage, NULL, &enemy.object);
+        SDL_RenderCopy(renderer, playerImage, NULL, &player.object);
+        SDL_RenderPresent(renderer);
+        //player.draw(renderer);
         SDL_Delay(100);
         player.move(moveX,moveY);
+        if(enemy.object.x < WIDTH/3)
+        {
+            enemyFollowX=enemyMoveAmount;
+        }
+        else if(enemy.object.x > (WIDTH/3) * 2)
+        {
+            enemyFollowX=-enemyMoveAmount;
+        }
+        else if(abs(enemy.object.x-player.object.x) <= 5 || abs(enemy.object.y-player.object.y) <= 5)
+        {
+                if(player.object.x-enemy.object.x < 0)
+                    enemyFollowX = -enemyMoveAmount;
+                else
+                    enemyFollowX = enemyMoveAmount;
+                if(player.object.y-enemy.object.y < 0)
+                    enemyFollowY = -enemyMoveAmount;
+                else
+                    enemyFollowY = enemyMoveAmount;
+        }
+        else
+        {
+            enemyFollowY = 0;
+        }
+        enemy.move(enemyFollowX,enemyFollowY);
         moveX = 0;
         moveY = 0;
-
     }
 
     SDL_DestroyWindow( window );
