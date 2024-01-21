b671fffcef4524ac05c95126904211c3ab768eda
The Great Macraft <156778003+TheGreatMacraft@users.noreply.github.com>
2024-01-21 09:09:19 +0100

    Update main.cpp

--- SDL/src/main.cpp
+++ SDL/src/main.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
-void rect()
+class Player{
+    public:
+    SDL_Rect object;
+    void size(int w, int h);
+    void move(int x, int y);
+    void setPosition(int x, int y);
+    void draw(SDL_Renderer *renderer);
+};
+
+void Player::size(int w, int h)
 {
-    SDL_Rect rectangle;
-    rectangle.x = 0;
-    rectangle.y = 0;
-    rectangle.w = 32;
-    rectangle.h = 32;
+    object.w = w;
+    object.h = h;
+}
+
+void Player::move(int x, int y)
+{
+    object.x += x;
+    object.y += y;
+}
+
+void Player::setPosition(int x, int y)
+{
+    object.x = x;
+    object.y = y;
+}
+
+void Player::draw(SDL_Renderer *renderer)
+{
+    SDL_SetRenderDrawColor(renderer,255,255,255,255);
+    SDL_RenderFillRect(renderer, &object);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderPresent(renderer);
 }
 
 const int WIDTH = 800, HEIGHT = 600;
@@ -20,34 +46,73 @@ int main( int argc, char* argv[] )
 
     if (SDL_Init(SDL_INIT_EVERYTHING))
     {
-        std::cout << "sdl init rahlo ne dela " << SDL_GetError() << "\n";
+        std::cout << "Could not innit: " << SDL_GetError() << "\n";
         return 1;
     }
 
-    SDL_Window* window = SDL_CreateWindow( "GAME", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE);
+    SDL_Window *window = SDL_CreateWindow( "GAME", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_RESIZABLE);
 
-    if (window == nullptr)
+    if (nullptr == window)
     {
-        std::cout << "Napaka pri ustvarjanju okna: " << SDL_GetError( ) << std::endl;
+        std::cout << "Could not create window: " << SDL_GetError( ) << std::endl;
         return 1;
     }
 
-    SDL_Event windowEvent;
-
-    while (true)
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (nullptr == renderer)
     {
-        if (SDL_PollEvent(&windowEvent))
+        std::cout << "Cold not create renderer: " << SDL_GetError() << std::endl;
+    }
+
+    SDL_Event event;
+
+    Player player;
+    player.size(20,20);
+    player.setPosition(WIDTH/2 - player.object.w/2, HEIGHT/2 - player.object.h/2);
+
+    int moveX, moveY, moveAmount = 10;
+
+    bool running = true;
+    while (running)
+    {
+        while(SDL_PollEvent(&event))
         {
-            if (SDL_QUIT == windowEvent.type)
-            {
-                break;
+            if (SDL_QUIT == event.type)
+            { 
+                running = false;
             }
+            if(event.type == SDL_KEYDOWN)
+            {
+                if(*SDL_GetKeyName(event.key.keysym.sym) == 'W')
+                {
+                    moveY = -moveAmount;
+                }
+                else if(*SDL_GetKeyName(event.key.keysym.sym) == 'S')
+                {
+                    moveY = moveAmount;
+                }
+                if(*SDL_GetKeyName(event.key.keysym.sym) == 'A')
+                {
+                    moveX = -moveAmount;
+                }
+                else if(*SDL_GetKeyName(event.key.keysym.sym) == 'D')
+                {
+                    moveX = moveAmount;
+                }
+            }
+
         }
+
+        SDL_RenderClear(renderer);
+        player.draw(renderer);
+        SDL_Delay(100);
+        player.move(moveX,moveY);
+        moveX = 0;
+        moveY = 0;
+
     }
 
-    SDL_RenderDrawRect()
-
-    SDL_DestroyWindow(window);
+    SDL_DestroyWindow( window );
     SDL_Quit( );
 
     return EXIT_SUCCESS;
